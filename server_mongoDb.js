390689382385a2211a4defa9b7aea131f9dba4a5
martipurull <91315241+martipurull@users.noreply.github.com>
2022-01-14 12:18:49 +0100

    adds users

--- server_mongoDb.js
+++ server_mongoDb.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import ProductsRouter from './services/mongo_services/index.js'
 import CartRouter from './services/mongo_services/cart/index.js'
+import UserRouter from './services/mongo_services/users/index.js'
 import { badRequestHandler, unauthorisedHandler, notFoundHandler, genericErrorHandler } from './errorHandlers.js'
 
 const server = express()
---
+++ services/mongo_services/users/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+// import UserSchema from './schema.js'
+import createHttpError from 'http-errors'
+import q2m from 'query-to-mongo'
+
+const UserRouter = express.Router()
+
+//
+
+
+
+
+
+
+export default UserRouter
