58fa75a8002c568c6202d372fb4428baacb844b1
martipurull <91315241+martipurull@users.noreply.github.com>
2022-01-14 15:24:04 +0100

    adds cart schema

--- services/mongo_services/cart/schema.js
+++ services/mongo_services/cart/schema.js
@@ -4,8 +4,11 @@ const { Schema, model } = mongoose
 
 const CartSchema = new Schema(
     {
-
-    }
+        userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        products: [{ name: String, price: Number, quantity: Number, originalId: Schema.Types.ObjectId }],
+        status: { type: String, required: true, enum: ["active", "paid"] }
+    },
+    { timestamps: true }
 )
 
 export default model("Cart", CartSchema)
