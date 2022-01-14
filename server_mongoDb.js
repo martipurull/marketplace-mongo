11bd0a8ce5cbaed9c0162ec32d71a80862fda242
martipurull <91315241+martipurull@users.noreply.github.com>
2022-01-14 15:37:47 +0100

    adds cart router

--- server_mongoDb.js
+++ server_mongoDb.js
@@ -1,17 +1,11 @@
-/** @format */
-
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import mongoose from "mongoose";
 import ProductsRouter from "./services/mongo_services/index.js";
+import CartRouter from "./services/mongo_services/cart/index.js"
 import UserRouter from "./services/mongo_services/users/index.js";
-import {
-    badRequestHandler,
-    unauthorisedHandler,
-    notFoundHandler,
-    genericErrorHandler,
-} from "./errorHandlers.js";
+import { badRequestHandler, unauthorisedHandler, notFoundHandler, genericErrorHandler } from "./errorHandlers.js";
 
 const server = express();
 
