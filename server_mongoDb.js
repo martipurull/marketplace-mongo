53bc4f96ed9f01a7b2e80bb179e0e62813bae051
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-14 13:34:46 +0100

    Crud for users completed.

--- server_mongoDb.js
+++ server_mongoDb.js
@@ -1,40 +1,47 @@
-import express from 'express'
-import listEndpoints from 'express-list-endpoints'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import ProductsRouter from './services/mongo_services/index.js'
-import CartRouter from './services/mongo_services/cart/index.js'
-import UserRouter from './services/mongo_services/users/index.js'
-import { badRequestHandler, unauthorisedHandler, notFoundHandler, genericErrorHandler } from './errorHandlers.js'
+/** @format */
 
-const server = express()
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import mongoose from "mongoose";
+import ProductsRouter from "./services/mongo_services/index.js";
+import UserRouter from "./services/mongo_services/users/index.js";
+import {
+  badRequestHandler,
+  unauthorisedHandler,
+  notFoundHandler,
+  genericErrorHandler,
+} from "./errorHandlers.js";
 
-const port = process.env.PORT || 3001
+const server = express();
+
+const port = process.env.PORT || 3001;
 
 //middleware
-server.use(cors())
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 
 //endpoints
-server.use('/products', ProductsRouter)
+server.use("/products", ProductsRouter);
+server.use("/users", UserRouter);
 
 //error handlers
-server.use(badRequestHandler)
-server.use(unauthorisedHandler)
-server.use(notFoundHandler)
-server.use(genericErrorHandler)
+server.use(badRequestHandler);
+server.use(unauthorisedHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
-mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connect(process.env.MONGO_CONNECTION);
 
 mongoose.connection.on("connected", () => {
-    console.log("Connected to Mongo!")
-})
+  console.log("Connected to Mongo!");
+});
 
 server.listen(port, () => {
-    console.table(listEndpoints(server))
-    console.log(`Server running on port: ${ port }`)
-})
+  console.table(listEndpoints(server));
+  console.log(`Server running on port: ${port}`);
+});
 
-mongoose.connection.on("error", err => {
-    console.log(err)
-})
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
